0a759c99a026c6c2d9d22ff2be65cf8d5551e47f
Zavaar Shah <thatziv@gmail.com>
2023-02-27 12:50:38 -0500

    href security

--- src/components/Contributions.js
+++ src/components/Contributions.js
@@ -7,7 +7,7 @@ export default function Contributions(props) {
         <Grid container justifyContent="space-between" alignItems="center">
             {props.repos.map((val, index) => <Grid item>
                 <Tooltip placement="top" title={"https://github.com/" + val}>
-                    <a target="_blank" href={"https://github.com/" + val}>
+                    <a target="_blank" rel="noreferrer" href={"https://github.com/" + val}>
                         <img alt={`${props.title} #${index + 1}`} className="Media" src={`https://gh-card.dev/repos/${val}.svg?fullname=`} />
                     </a>
                 </Tooltip>
