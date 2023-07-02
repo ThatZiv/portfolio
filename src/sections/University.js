e7c749fecb3c30af14f11b9417a84dc4b571f7e9
ThatZiv <thatziv@gmail.com>
2023-07-02 11:26:21 -0400

    coursework refactoring

--- src/sections/University.js
+++ src/sections/University.js
@@ -31,23 +31,55 @@ It is Michigan's third-largest university. Founded in 1868, Wayne State consists
 export default Section;
 
 function Coursework() {
-  const coursePage = (id) =>
-    `https://bulletins.wayne.edu/search/?P=${encodeURIComponent(id)}`;
-  // TODO: find better way to do courseworks
+  class Course {
+    /**
+     * @constructor
+     * @param {Object} options - The options for creating a new person.
+     * @param {string} options.name - The name of the person.
+     * @param {string} options.title - The title of the person.
+     * @param {string} options.description - The description of the person.
+     */
+    constructor({ name, title, description }) {
+      this.name = name;
+      this.title = title;
+      this.description = description;
+      let course = {
+        title,
+        content: `## [${name}](${this.getCoursePage()})
+${description}`,
+      };
+      return course;
+    }
+    getCoursePage() {
+      return `https://bulletins.wayne.edu/search/?P=${encodeURIComponent(
+        this.name
+      )}`;
+    }
+  }
   const courses = [
-    {
-      title: "CSC 1100",
-      content: () =>
-        `[Problem Solving and Programming](${coursePage(this.title)})`,
-    },
-    {
-      title: "CSC 1500",
-      content: "Fundamental Structures in Computer Science.",
-    },
-    {
-      title: "CSC 2200",
-      content: "Computer Science II - Data Structures and Algorithms.",
-    },
+    new Course({
+      name: "CSC 1100",
+      title: "Programming and Problem Solving",
+      description:
+        "Problem solving with algorithms, and their realization as computer programs using a structured, general purpose programming language; data types, operators, expressions, assignment, input and output, selection and repetition control structures; modularity and procedural abstraction using functions with parameters; structured data types, arrays, pointers and strings.Problem solving with algorithms, and their realization as computer programs using a structured, general purpose programming language; data types, operators, expressions, assignment, input and output, selection and repetition control structures; modularity and procedural abstraction using functions with parameters; structured data types, arrays, pointers and strings.",
+    }),
+    new Course({
+      name: "CSC 1500",
+      title: "Fundamental Structures in Computer Science",
+      description:
+        "Introduction to fundamental control and data structures in computer science such as algorithms and complexity; recursive algorithms; program correctness using the predicate calculus; reasoning about algorithms using mathematical induction; divide and conquer algorithms; recurrence relations; set properties and their computation; and computing with relations. Graph properties and their computation, and tree properties and their computation, will be covered if time permits.",
+    }),
+    new Course({
+      name: "CSC 2110",
+      title: "...",
+      description: "...", // TODO get server1 back up so i can continue
+    }),
+    new Course({
+      name: "CSC 2200",
+      title: "Data Structures and Algorithms",
+      description:
+        "Design and implementation of fundamental abstract data types of computer science (such as stacks, queues, trees, lists, hashing, and graphs), using an object-oriented language. Programming requirements include the implementation of abstract data types using arrays and dynamic links; recursion; sorting and searching; hashing; and string processing. Introduction to algorithm analysis.",
+    }),
   ];
   console.log(courses);
   return <Generic entries={courses} />;
