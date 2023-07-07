81190c31d133755cc4308df1a87a38002c973cc8
ThatZiv <thatziv@gmail.com>
2023-07-07 19:23:47 -0400

    added ap csp projects

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from "./Theme"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "./Theme";
 //ctx
-import { UserProvider } from "./contexts"
-import { ViewportProvider } from './contexts/viewport'; // will be used in the future
-import { HashRouter as Router } from 'react-router-dom';
+import { UserProvider } from "./contexts";
+import { ViewportProvider } from "./contexts/viewport"; // will be used in the future
+import { HashRouter as Router } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <UserProvider>
-      <ThemeProvider theme={theme}>
-        <CssBaseline>
-          <Router>
-            <App />
-          </Router>
-        </CssBaseline>
-      </ThemeProvider>
+      <ViewportProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline>
+            <Router>
+              <App />
+            </Router>
+          </CssBaseline>
+        </ThemeProvider>
+      </ViewportProvider>
     </UserProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 console.log(`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`
@@ -44,11 +46,10 @@ console.log(`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`
 \`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`
 \`\`\`\`\`\`\`\`\` to my site \`\`\`\`\`\`\`\`\`
 \`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`\`
-`)
-
+`);
 
 if ("serviceWorker" in navigator && process.env.NODE_ENV === "production") {
-  navigator.serviceWorker.register("/service-worker.js")
+  navigator.serviceWorker.register("/service-worker.js");
 }
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/sections/School.js
+++ src/sections/School.js
@@ -1,5 +1,9 @@
+import { Box, Button, Grid, Typography } from "@material-ui/core";
 import Generic from "../components/Generic";
 import Markdown from "../components/Markdown";
+import { useViewport } from "../contexts/viewport";
+import SocialMedia from "../components/SocialMedia";
+
 const GPA = "3.91";
 /**
  * @type {import("../types/section").Section}
@@ -41,51 +45,123 @@ function Organizations() {
 }
 
 function Coursework() {
+  const { width } = useViewport(); // used bc embeds on mobile screw up entire page
   return (
-    <Generic
-      entries={[
-        {
-          title: "AP® Computer Science Principles",
-          content:
-            "Learn the principles that underlie the science of computing and develop the thinking skills that computer scientists use. You'll work on your own and as part of a team to creatively address real-world issues using the tools and processes of computation.",
-        },
-        {
-          title: "AP® Macroeconomics",
-          content:
-            "Explore the principles of economics that apply to an economic system as a whole. You'll use graphs, charts, and data to analyze, describe, and explain economic concepts.",
-        },
-        {
-          title: "AP® Microeconomics",
-          content:
-            "Study the principles of economics that apply to the behavior of individuals within an economic system. You'll use graphs, charts, and data to analyze, describe, and explain economic concepts.",
-        },
-        {
-          title: "AP® Psychology",
-          content:
-            "Explore the ideas, theories, and methods of the scientific study of behavior and mental processes. You'll examine the concepts of psychology through reading and discussion and you'll analyze data from psychological research studies.",
-        },
-        {
-          title: "AP® Calculus BC",
-          content:
-            "Explore the concepts, methods, and applications of differential and integral calculus, including topics such as parametric, polar, and vector functions, and series. You'll perform experiments and investigations and solve problems by applying your knowledge and skills. ",
-        },
-        {
-          title: "AP® Environmental Science",
-          content:
-            "Explore and investigate the interrelationships of the natural world and analyze environmental problems, both natural and human-made. You'll take part in laboratory investigations and field work.",
-        },
-        {
-          title: "AP® Physics C: Mechanics",
-          content:
-            "Explore concepts such as kinematics; Newton's laws of motion, work, energy, and power; systems of particles and linear momentum; rotation; oscillations; and gravitation. You'll do hands-on laboratory work and in-class activities to investigate phenomena and use calculus to solve problems.",
-        },
-        {
-          title: "AP® Physics C: Electricity and Magnetism",
-          content:
-            "Explore concepts such as electrostatics; conductors, capacitors, and dielectrics; electric circuits; magnetic fields; and electromagnetism. You'll do hands-on laboratory work to investigate phenomena.",
-        },
-      ]}
-    />
+    <>
+      <Grid container justifyContent="center" alignItems="flex-start">
+        <Generic
+          entries={[
+            {
+              title: "AP® Computer Science Principles",
+              content: `Learn the principles that underlie the science of computing and develop the thinking skills that computer scientists use. 
+You'll work on your own and as part of a team to creatively address real-world issues using the tools and processes of computation. Coursework for this course were written in **JavaScript**.
+## The following are projects I created during the course:`,
+            },
+          ]}
+        />
+        <br />
+        <Grid md={6} item>
+          <Typography variant="h3">Hackathon Project</Typography>
+          <Typography
+            style={{
+              fontStyle: "italic",
+              color: "gray",
+            }}
+            variant="subtitle1"
+          >
+            The overall purpose of this program is to visualize the vast scale
+            of the COVID-19 virus using easy to understand metrics and helpful
+            visuals.
+          </Typography>
+          <br />
+          {width > 590 ? (
+            <iframe
+              width={392}
+              height={620}
+              style={{ border: 0 }}
+              title="Speed typing"
+              src="https://studio.code.org/projects/applab/rDPTnt0C8e1cAMNHEVY4qE6LypS_ddRIgJEbdswHRws/embed"
+            ></iframe>
+          ) : (
+            <SocialMedia
+              name="View Project"
+              icon="fa-solid fa-arrow-up-right-from-square"
+              url="https://studio.code.org/projects/applab/rDPTnt0C8e1cAMNHEVY4qE6LypS_ddRIgJEbdswHRws/view"
+              showName
+            />
+          )}
+        </Grid>
+        <Grid md={6} item>
+          <Typography variant="h3">AP® Final Project</Typography>
+          <Typography
+            style={{
+              fontStyle: "italic",
+              color: "gray",
+            }}
+            variant="subtitle1"
+          >
+            The purpose of the "SpeedTyping" game is to train and measure a
+            user's typing speed based on randomly generated words.
+          </Typography>
+          <br />
+          {width > 590 ? (
+            <iframe
+              width={392}
+              height={620}
+              style={{ border: 0 }}
+              title="Speed typing"
+              src="https://studio.code.org/projects/applab/gfi_E_2YNQrmHEsOXV50OU8Jl3jtQdhZYXK8-P3kS2Q/embed"
+            ></iframe>
+          ) : (
+            <SocialMedia
+              name="View Project"
+              icon="fa-solid fa-arrow-up-right-from-square"
+              url="https://studio.code.org/projects/applab/gfi_E_2YNQrmHEsOXV50OU8Jl3jtQdhZYXK8-P3kS2Q/view"
+              showName
+            />
+          )}
+        </Grid>
+        <Generic
+          entries={[
+            {
+              title: "AP® Macroeconomics",
+              content:
+                "Explore the principles of economics that apply to an economic system as a whole. You'll use graphs, charts, and data to analyze, describe, and explain economic concepts.",
+            },
+            {
+              title: "AP® Microeconomics",
+              content:
+                "Study the principles of economics that apply to the behavior of individuals within an economic system. You'll use graphs, charts, and data to analyze, describe, and explain economic concepts.",
+            },
+            {
+              title: "AP® Psychology",
+              content:
+                "Explore the ideas, theories, and methods of the scientific study of behavior and mental processes. You'll examine the concepts of psychology through reading and discussion and you'll analyze data from psychological research studies.",
+            },
+            {
+              title: "AP® Calculus BC",
+              content:
+                "Explore the concepts, methods, and applications of differential and integral calculus, including topics such as parametric, polar, and vector functions, and series. You'll perform experiments and investigations and solve problems by applying your knowledge and skills. ",
+            },
+            {
+              title: "AP® Environmental Science",
+              content:
+                "Explore and investigate the interrelationships of the natural world and analyze environmental problems, both natural and human-made. You'll take part in laboratory investigations and field work.",
+            },
+            {
+              title: "AP® Physics C: Mechanics",
+              content:
+                "Explore concepts such as kinematics; Newton's laws of motion, work, energy, and power; systems of particles and linear momentum; rotation; oscillations; and gravitation. You'll do hands-on laboratory work and in-class activities to investigate phenomena and use calculus to solve problems.",
+            },
+            {
+              title: "AP® Physics C: Electricity and Magnetism",
+              content:
+                "Explore concepts such as electrostatics; conductors, capacitors, and dielectrics; electric circuits; magnetic fields; and electromagnetism. You'll do hands-on laboratory work to investigate phenomena.",
+            },
+          ]}
+        />
+      </Grid>
+    </>
   );
 }
 
@@ -110,3 +186,5 @@ function Awards() {
     />
   );
 }
+
+function Projects() {}
